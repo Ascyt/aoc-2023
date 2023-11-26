883880db9aea04f22c39e007f4f259f286fc8d38
Ascyt <filip.schauer07@gmail.com>
2023-11-26 14:22:45 +0100

    Improved requests to use loop

--- GetInput.py
+++ GetInput.py
@@ -2,29 +2,56 @@ import requests
 import re
 from Tools import *
 import SensitiveData
+from datetime import datetime
+import time
+import sys
 
 cookies = {}
 
 def get_html(url)->str:
-    try:
-        response = requests.get(url, cookies=cookies)
-        response.raise_for_status()
-        return response.text
-    except requests.HTTPError as ex:
-        print(f"HTTP error occurred: {ex}")
-    except requests.RequestException as ex:
-        print(f"Error occurred: {ex}")
+    response = requests.get(url, cookies=cookies)
+    return response
 
 print_title('GET INPUT')
 
 cookies = dict(session=SensitiveData.SESSION_COOKIE)
-year:int = input('Year: ')
-day:int = input('Day: ')
+
+now = datetime.now()
+
+day:int = now.day
+year:int = now.year
+
+print(f'Year {year} | Day {day}')
 
 print()
 
 print('Getting HTML...')
-html:str = get_html(f'https://adventofcode.com/{year}/day/{day}')
+
+retries:int = 0
+html:str = None
+
+def clear_line():
+    sys.stdout.write("\033[F")  # Cursor up one line
+    sys.stdout.write("\033[K")  # Clear to the end of line
+
+while True:
+    response:str = get_html(f'https://adventofcode.com/{year}/day/{day}')
+    if response.status_code == 200:
+        print('Successfully fetched HTML')
+        html = response.text
+        break
+
+    if response.status_code == 404:
+        retries += 1
+        clear_line()
+        print(f'Not found. (#{retries})')
+        time.sleep(1)
+        clear_line()
+        print(f'Not found. (#{retries}) Trying again ...')
+        continue
+
+    raise requests.HTTPError(response=response)
+
 
 print('Extracting article...')
 match = re.search('<article class="day-desc">(.*?)</article>', html, re.DOTALL)
